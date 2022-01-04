9f04548ff6a09a5340256ad409d9e37815a37b67
cointum <74637361+cointum@users.noreply.github.com>
2022-01-04 19:21:46 +0530

    question 1

--- timeConversion.go
+++ timeConversion.go
@@ -8,15 +8,21 @@ import (
 )
 
 func main() {
-	y := 30099458
+
+	mil:=30099458424
+
+	y := (mil/1000)
+	// convert int to string 
 	q := strconv.Itoa(y)
 	var r string = "s"
+
 	t := q + r
 
 	u, _ := time.ParseDuration(t)
+	//conver hour into days
 	var totalDays float64 = (u.Hours()) / 24
 
-	fmt.Printf("Milliseconds to christmas: %d\n", u.Milliseconds())
+	fmt.Printf("Milliseconds to christmas: %d\n", mil)
 	fmt.Println("Seconds to christmas: ", y)
 
 	fmt.Printf("Minutes to christmas: %.f\n", math.Round(u.Minutes()))
